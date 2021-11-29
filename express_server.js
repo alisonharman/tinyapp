3ee56cee762bcf8f143de86b2d5cb6b74de167c6
Alison Harman <alison_harman@sympatico.ca>
2021-11-29 16:47:41 +0000

    implemented redirect after form submission and redirect to page after requesting shortURL

--- express_server.js
+++ express_server.js
@@ -22,8 +22,12 @@ app.get("/", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+ // console.log(req.body);  // Log the POST request body to the console
+  //res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body["longURL"];
+  console.log('URLDATABASE', urlDatabase);
+  res.redirect(`/urls/:${shortURL}`);
 });
 /*
 app.get("/urls.json", (req, res) => {
@@ -45,6 +49,11 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  // need to deal with case with input shortURL params not valid
+  res.redirect(longURL);
+});
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
