b80fcc015790e847cc5fc48e028b439fd3c0a9d6
Alison Harman <alison_harman@sympatico.ca>
2021-12-01 03:04:21 +0000

    passed username as part of templateVars to all views using _header.ejs partial

--- express_server.js
+++ express_server.js
@@ -1,14 +1,16 @@
 const express = require("express");
+const cookieParser = require('cookie-parser')
 const app = express();
 const PORT = 8080; // default port 8080
 
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser())
 
 app.set("view engine", "ejs");
 
 function generateRandomString() {
-  const result = Math.random().toString(36).substring(2,8);
+  const result = Math.random().toString(36).substring(2, 8);
   return result;
 }
 
@@ -22,13 +24,14 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  //const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = { username: req.cookies["username"], shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
 
 app.post("/urls", (req, res) => {
- // console.log(req.body);  // Log the POST request body to the console
+  // console.log(req.body);  // Log the POST request body to the console
   //res.send("Ok");         // Respond with 'Ok' (we will replace this)
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body["longURL"];
@@ -40,7 +43,7 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   console.log('URLDATABASE', urlDatabase);
   res.redirect('/urls');
 })
-  
+
 
 
 /*
@@ -50,12 +53,20 @@ app.get("/urls.json", (req, res) => {
 */
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  //const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    username: req.cookies["username"],
+    urls: urlDatabase
+  };
+  console.log(templateVars);
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"],
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.post('/login', (req, res) => {
