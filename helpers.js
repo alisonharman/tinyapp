8e807c18902bab9f74e371db00438be2a2b44232
Alison Harman <alison_harman@sympatico.ca>
2021-12-07 16:21:42 +0000

    fix module exports for getUserByEmail function

--- helpers.js
+++ helpers.js
@@ -1,8 +1,8 @@
 const getUserByEmail = (email, users) => {
   const userId = Object.keys(users).find(id => users[id].email === email)
-  if (!userId) return null
+  if (!userId) return undefined
   const user = users[userId]
   return { id: userId, ...user }
 }
 
-module.exports = getUserByEmail;
+module.exports = { getUserByEmail };
