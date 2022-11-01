3dbec3edde5ea4adee4266eacb045b71dab24691
Alison Harman <alison_harman@sympatico.ca>
2022-11-01 19:34:38 -0400

    fix formatting

--- helpers.js
+++ helpers.js
@@ -1,11 +1,11 @@
 const getUserByEmail = (email, users) => {
-  const userId = Object.keys(users).find(id => users[id].email === email)
-  if (!userId) return undefined
-  const user = users[userId]
-  return { id: userId, ...user }
-}
+  const userId = Object.keys(users).find((id) => users[id].email === email);
+  if (!userId) return undefined;
+  const user = users[userId];
+  return { id: userId, ...user };
+};
 
-const generateRandomString = function() {
+const generateRandomString = function () {
   const result = Math.random().toString(36).substring(2, 8);
   return result;
 };
@@ -36,5 +36,9 @@ const allowedAccess = function (id, shortURL, urlDatabase) {
   return false;
 };
 
-
-module.exports = { getUserByEmail, generateRandomString, urlsForUser, allowedAccess };
+module.exports = {
+  getUserByEmail,
+  generateRandomString,
+  urlsForUser,
+  allowedAccess,
+};
